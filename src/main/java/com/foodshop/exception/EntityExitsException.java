f41061fbe842a750009e186733263e2a385f71b0
levanphuc18 <levanphuc18112001@gmail.com>
2025-09-20 18:06:22 +0700

    Delete unused files "EntityExitsException"

--- src/main/java/com/foodshop/exception/EntityExitsException.java
+++
@@ -1,7 +0,0 @@
-package com.foodshop.exception;
-
-public class EntityExitsException extends GlobalException {
-    public EntityExitsException(String message, String code) {
-        super(code, message);
-    }
-}
